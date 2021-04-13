0d3cc91d4e7f214a4ab06bab2518edbae4c37d52
khaled <lga.sig.khaled@gmail.com>
2021-04-14 00:32:27 +0200

    fixed-bug && delete-files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 import React, { Component } from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
+import Div from './Component/Div.js';
 
 class App extends Component {
 	render() {
-		return <div className="App" />;
+		return (
+			<HashRouter>
+				<React.Suspense>
+					<Switch>
+						<Route exact path="/" name="Home" component={Div} />
+						<Route exact path="/login" name="Home" component={Div} />
+					</Switch>
+				</React.Suspense>
+			</HashRouter>
+		);
 	}
 }
 
