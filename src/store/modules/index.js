c91443e2460e92abbabff28c0577d975add3a853
khaled <lga.sig.khaled@gmail.com>
2021-04-16 00:56:11 +0200

    init store redux

---
+++ src/store/modules/index.js
@@ -0,0 +1,6 @@
+import { combineEpics } from 'redux-observable';
+import { combineReducers } from 'redux';
+
+export const rootEpic = combineEpics();
+
+export const rootReducer = combineReducers({});
